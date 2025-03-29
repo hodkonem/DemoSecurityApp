50d249c48df23f193a74be3ae6750172d1bb4832
Misha <mikhaillat@yandex.ru>
2025-03-29 20:23:49 +0300

    Initial commit

---
+++ src/main/java/ru/latypov/DemoSecurityApp/DemoSecurityAppApplication.java
@@ -0,0 +1,13 @@
+package ru.latypov.DemoSecurityApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSecurityAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSecurityAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/latypov/DemoSecurityApp/DemoSecurityAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.latypov.DemoSecurityApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSecurityAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
