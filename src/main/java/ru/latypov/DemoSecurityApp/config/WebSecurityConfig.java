aaca7c09e8100405805cf7153714785795702c40
Misha <mikhaillat@yandex.ru>
2025-03-31 17:54:43 +0300

    Добавил шифрование пароля при регистрации нового пользователя

--- src/main/java/ru/latypov/DemoSecurityApp/config/WebSecurityConfig.java
+++ src/main/java/ru/latypov/DemoSecurityApp/config/WebSecurityConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import ru.latypov.DemoSecurityApp.services.PersonDetailsService;
@@ -45,6 +45,6 @@ public class WebSecurityConfig {
 
     @Bean
     PasswordEncoder getPasswordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/ru/latypov/DemoSecurityApp/services/RegistrationService.java
+++ src/main/java/ru/latypov/DemoSecurityApp/services/RegistrationService.java
@@ -1,6 +1,7 @@
 package ru.latypov.DemoSecurityApp.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.latypov.DemoSecurityApp.models.Person;
@@ -9,14 +10,17 @@ import ru.latypov.DemoSecurityApp.repositories.PeopleRepository;
 @Service
 public class RegistrationService {
     private final PeopleRepository peopleRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public RegistrationService(PeopleRepository peopleRepository) {
+    public RegistrationService(PeopleRepository peopleRepository, PasswordEncoder passwordEncoder) {
         this.peopleRepository = peopleRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Transactional
     public void register(Person person) {
+        person.setPassword(passwordEncoder.encode(person.getPassword()));
         peopleRepository.save(person);
     }
 }
